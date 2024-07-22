b053a2eb1e5541aef990268d19e472ea554954b6
saadkhaliq15 <saadkhaliq15@gmail.com>
2024-07-22 15:17:49 +0500

    chore: Update page.tsx with new heading for AY Associates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       
       <h1 className="text-6xl font-bold">Welcome to AY Associates</h1>
+      <h2 className="text-6xl font-bold">Me hun AY Assosiates</h2>
     </div>
   );
 }
