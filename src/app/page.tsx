c47ea7b055d02d92944ab345944b1ee2fa9c2c95
saadkhaliq15 <saadkhaliq15@gmail.com>
2024-07-22 15:22:10 +0500

    chore: Remove unnecessary heading from page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       
       <h1 className="text-6xl font-bold">Welcome to AY Associates</h1>
-      <h2 className="text-6xl font-bold">Me hun AY Assosiates</h2>
     </div>
   );
 }
