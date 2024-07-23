3137e55d15532e62b7f5ed82fa994d1854332ab0
Javeria <jiyaaa1812@gmail.com>
2024-07-23 18:03:42 +0500

    Update global CSS and page styling
    - Import PT Serif font from Google Fonts in the global CSS file
    - Update the font weight of the page title in the Home component
    - Update the primary font family in the Tailwind config file
    
    Related to: #123

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="flex grow flex-col items-center self-stretch">
       <section className="relative flex h-[80vh] grow items-center justify-center self-stretch bg-hero bg-cover bg-center pt-3 sm:h-[90vh]">
         <div className="text-center text-white">
-          <h1 className="mb-4 px-4 text-3xl font-bold sm:text-6xl">
+          <h1 className="mb-4 px-4 text-4xl font-semibod sm:text-6xl">
           Building Nature Communities
           </h1>
           <p className="mb-8 px-6 text-sm sm:text-lg">
