130a79bc2c3252587d5242605ba2761f30f1cb5b
Saad Khan <saadkhaliq15@gmail.com>
2024-07-23 19:08:50 +0500

    [US-273] Implement Sitemap

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ContactPage = () => {
+  return <div className="flex grow flex-col items-center self-stretch">
+
+    
+  </div>;
+};
+
+export default ContactPage;
