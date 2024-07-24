68396fb2200e8c142bff6be097267a9299ac2112
Javeria <jiyaaa1812@gmail.com>
2024-07-24 13:35:39 +0500

    Updated Styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,22 +37,6 @@ export default function Home() {
       </section>
       <section className="About px-8 sm:px-48 py-12 flex flex-col sm:flex-row items-start justify-between bg-white gap-5 sm:gap-36 ">
         <SectionHeader />
-        <div className="flex flex-col gap-6 max-w-lg">
-          <p className="text-left text-gray-600 text-sm sm:text-md font-normal font-secondary leading-9">
-            Holistic Group specializes in building communities and providing end-to-end solutions to customers aspiring for a nature-centric lifestyle. We Design, Build, Develop & Manage these communities.
-            In five years, the successful delivery of four projects is a testament to our commitment to a Nature-Centric Lifestyle as a tangible and compelling reality for our valued customers.
-            We specialize in creating spaces that seamlessly blend with nature, offering bespoke designs for homes, unparalleled hospitality experiences, and thoughtful solutions for senior living.
-          </p>
-          <Link href="/">
-            <Button
-              variant="secondary"
-              size="lg"
-              className="shadow-xl bg-secondary text-white"
-            >
-              Learn More
-            </Button>
-          </Link>
-        </div>
       </section>
     </div>
   );
