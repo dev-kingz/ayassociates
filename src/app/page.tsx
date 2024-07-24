c67aabd9f0976b4e0b308a634a80918a8fbfc086
Javeria <jiyaaa1812@gmail.com>
2024-07-24 12:42:58 +0500

    Added and Updated the styling of Section Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import SectionHeader from "@/components/Header/SectionHeader";
 import { Button } from "@/components/ui/button"
+import SectionHeader from "@/components/Header/SectionHeader";
+import { Button } from "@/components/ui/button"
 import Link from "next/link";
 
 export default function Home() {
@@ -9,8 +11,10 @@ export default function Home() {
         <div className="text-center text-white">
           <h1 className="mb-4 px-4 text-4xl font-semibod sm:text-6xl">
             Building Nature Communities
+            Building Nature Communities
           </h1>
           <p className="mb-8 px-6 text-sm sm:text-lg">
+            Invest in Tranquility, Live in Luxury
             Invest in Tranquility, Live in Luxury
           </p>
           <div className="flex justify-center flex-row gap-6">
@@ -32,11 +36,29 @@ export default function Home() {
                 Learn More
               </Button>
             </Link>
+            <Link href="/">
+              <Button
+                variant="secondary"
+                size="lg"
+                className="shadow-xl bg-secondary text-white"
+              >
+                Enquire Now
+              </Button>
+            </Link>
+            <Link href="/">
+              <Button
+                variant="outline"
+                size="lg"
+                className="shadow-xl"
+              >
+                Learn More
+              </Button>
+            </Link>
           </div>
         </div>
       </section>
-      <section className="About px-8 sm:px-48 py-12 flex flex-col sm:flex-row items-start justify-between bg-white gap-5 sm:gap-36 ">
-        <SectionHeader title="About Us" subTitle="About Holistic Nature Homes" titleColor="text-secondary" subTitleColor="text-lime-950"/>
+      <section className="About px-24 py-12 flex self-stretch">
+        <SectionHeader/>
       </section>
     </div>
   );
--- src/components/Header/SectionHeader.tsx
+++ src/components/Header/SectionHeader.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const SectionHeader = ({title, subTitle, titleColor, subTitleColor}: {title: string, subTitle: string, titleColor: string, subTitleColor: string}) => {
+const SectionHeader = () => {
     return (
         <div className="w-full flex-col justify-start items-start inline-flex gap-3">
             <div className="flex justify-start items-center gap-4">
-                <div className="w-[75px] h-[1px] bg-secondary" />
-                <div className={`${titleColor} text-md font-normal font-secondary`}>{title}</div>
+                <div className="w-24 h-1 bg-secondary" />
+                <div className="text-secondary text-xl font-normal font-['Poppins']">About us</div>
             </div>
-            <div className={`self-stretch ${subTitleColor} text-3xl sm:text-4xl font-medium`}>{subTitle}</div>
+            <div className="self-stretch text-lime-950 text-4xl font-medium">About AY Associates Nature Homes</div>
         </div>
     );
 };
