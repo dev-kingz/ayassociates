627ceef76bc0d504b5b9b96bd42d9870ce09a3e7
Javeria <jiyaaa1812@gmail.com>
2024-07-24 13:34:32 +0500

    Updated About Us Section

--- src/components/Header/SectionHeader.tsx
+++ src/components/Header/SectionHeader.tsx
@@ -5,7 +5,7 @@ const SectionHeader = () => {
         <div className="w-full flex-col justify-start items-start inline-flex gap-3">
             <div className="flex justify-start items-center gap-4">
                 <div className="w-24 h-1 bg-secondary" />
-                <div className="text-secondary text-xl font-normal">About us</div>
+                <div className="text-secondary text-lg font-normal font-secondary">About us</div>
             </div>
             <div className="self-stretch text-lime-950 text-3xl sm:text-4xl font-medium">About AY Associates Nature Homes</div>
         </div>
