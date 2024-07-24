ae79af5a8dcc0dc7d7fa703a28d46a19f1d9dffb
Javeria Irfan <jiyaaa1812@gmail.com>
2024-07-24 18:49:53 +0500

    [US-213] Why Choose Us Grid (#20)
    * refactor: Update page to include Why Choose Us section with IconTextCard components
    
    * refactor: Update Why Choose Us section styling and content
    
    * Merge branch 'dev' into Why-Choose-Us-Grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
-import SectionHeader from "@/components/Header/SectionHeader";
+import IconTextCard from "@/components/Cards/IconTextCard";
 import { Button } from "@/components/ui/button"
 import Link from "next/link";
+import { MdSecurity } from "react-icons/md";
+import { GrUserManager } from "react-icons/gr";
+import { FaMapLocationDot, FaHouseChimney  } from "react-icons/fa6";
 
 export default function Home() {
   return (
@@ -9,8 +12,9 @@ export default function Home() {
         <div className="text-center text-white">
           <h1 className="mb-4 px-4 text-4xl font-semibod sm:text-6xl">
             Building Nature Communities
+            Building Nature Communities
           </h1>
-          <p className="mb-8 px-6 text-sm sm:text-lg">
+          <p className="mb-8 px-6 text-sm sm:text-lg font-secondary">
             Invest in Tranquility, Live in Luxury
           </p>
           <div className="flex justify-center flex-row gap-6">
@@ -32,11 +36,40 @@ export default function Home() {
                 Learn More
               </Button>
             </Link>
+            <Link href="/">
+              <Button
+                variant="secondary"
+                size="lg"
+                className="shadow-xl bg-secondary text-white"
+              >
+                Enquire Now
+              </Button>
+            </Link>
+            <Link href="/">
+              <Button
+                variant="outline"
+                size="lg"
+                className="shadow-xl"
+              >
+                Learn More
+              </Button>
+            </Link>
           </div>
         </div>
       </section>
-      <section className="About px-8 sm:px-48 py-12 flex flex-col sm:flex-row items-start justify-between bg-white gap-5 sm:gap-36 ">
-        <SectionHeader title="About Us" subTitle="About Holistic Nature Homes" titleColor="text-secondary" subTitleColor="text-lime-950"/>
+      <section className="WhyChooseUs px-12 py-8 lex flex-col items-center justify-center self-stretch ">
+        <div className="Header justify-center flex flex-col items-center gap-2 sm:gap-6">
+          <h2 className="text-2xl sm:text-4xl font-semibold text-secondary">Why Choose Us?</h2>
+          <p className="px-30 sm:px-80 text-center text-sm sm:text-md text-gray-500">
+            We provide end to end solutions & undertake Design, Approval, Procurement, Construction and Maintenance of all projects with our in house team of experts
+          </p>
+        </div>
+        <div className="Cards py-8 flex flex-col sm:flex-row items-center justify-center gap-4 sm:gap-8">
+          <IconTextCard Icon={MdSecurity} title="Secured & Fenced" text="Round-the-Clock Safety and Security for All" bgColor="bg-secondary" />
+          <IconTextCard Icon={GrUserManager} title="Managed & Maintained" text="Hassle Free & Cost Effective" bgColor="bg-white" />
+          <IconTextCard Icon={FaMapLocationDot} title="Prime Location & Smart Investment" text="Connectivity & Well-Thought-Out Returns" bgColor="bg-secondary" />
+          <IconTextCard Icon={FaHouseChimney } title="Integrated Resort Community Estates" text="Robust Amenities & Infrastructure" bgColor="bg-white" />
+        </div>
       </section>
     </div>
   );
