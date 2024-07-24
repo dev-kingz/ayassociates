813512f6a69f7b36d5ab76d0b475664c3313ce31
Javeria <jiyaaa1812@gmail.com>
2024-07-24 12:49:30 +0500

    - Added About Us Section to Home Page

--- src/components/Header/SectionHeader.tsx
+++ src/components/Header/SectionHeader.tsx
@@ -5,9 +5,9 @@ const SectionHeader = () => {
         <div className="w-full flex-col justify-start items-start inline-flex gap-3">
             <div className="flex justify-start items-center gap-4">
                 <div className="w-24 h-1 bg-secondary" />
-                <div className="text-secondary text-xl font-normal font-['Poppins']">About us</div>
+                <div className="text-secondary text-xl font-normal">About us</div>
             </div>
-            <div className="self-stretch text-lime-950 text-4xl font-medium">About AY Associates Nature Homes</div>
+            <div className="self-stretch text-lime-950 text-3xl sm:text-4xl font-medium">About AY Associates Nature Homes</div>
         </div>
     );
 };
