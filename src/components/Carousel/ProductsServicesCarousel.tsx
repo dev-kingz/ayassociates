6ea0eb8c037cf9c455c1b39d4930cb0196982cbf
Javeria <jiyaaa1812@gmail.com>
2024-07-25 19:31:38 +0500

    chore: Update Home page layout and remove unnecessary code

--- src/components/Carousel/ProductsServicesCarousel.tsx
+++ src/components/Carousel/ProductsServicesCarousel.tsx
@@ -8,7 +8,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import Autoplay from "embla-carousel-autoplay"
+import Autoplay from "embla-carousel-autoplay";
 
 const images = [
   "/gallery/images/Products-Services-Carousel-Picture1.jpg",
@@ -19,19 +19,20 @@ const images = [
 export function ProductsServicesCarousel() {
   const plugin = React.useRef(
     Autoplay({ delay: 2000, stopOnInteraction: true })
-  )
+  );
   return (
-    <Carousel 
-    plugins={[plugin.current]}
-    onMouseEnter={plugin.current.stop}
-    onMouseLeave={plugin.current.reset}
-    className=" w-[310px] sm:w-[440px] h-[400px]">
+    <Carousel
+      plugins={[plugin.current]}
+      onMouseEnter={plugin.current.stop}
+      onMouseLeave={plugin.current.reset}
+      className="w-[310px] sm:w-[430px] h-[400px]"
+    >
       <CarouselContent>
         {images.map((image, index) => (
-          <CarouselItem key={index} className=" h-full">
-            <div className="w-full h-full"> 
-              <Card className="w-full h-[360px] sm:h-[500px]"> 
-                <CardContent className="flex items-center justify-center h-full p-0"> {/* Removed padding */}
+          <CarouselItem key={index} className="h-full">
+            <div className="w-full h-full">
+              <Card className="w-full h-[360px] sm:h-[500px]">
+                <CardContent className="flex items-center justify-center h-full p-0">
                   <img src={image} alt={`Image ${index + 1}`} className="rounded-lg w-full h-full object-cover" />
                 </CardContent>
               </Card>
